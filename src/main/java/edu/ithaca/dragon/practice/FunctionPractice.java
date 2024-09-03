ccd36682dfe7e2eb59d8d10b855cc70d7609890d
quinnsmith <qsmith@ithaca.edu>
2024-09-03 11:18:47 -0400

    salsesndone

--- src/main/java/edu/ithaca/dragon/practice/FunctionPractice.java
+++ src/main/java/edu/ithaca/dragon/practice/FunctionPractice.java
@@ -32,7 +32,10 @@ public class FunctionPractice {
      * @return the final price at register of the given item after discount and tax applied
      */
     public static double calcSalePrice(double originalPrice, double discountPercent, double salesTax){
-        throw new RuntimeException("Not Implemented");
+        discountPercent /=100;
+        salesTax/=100;
+        double newPrice = originalPrice - (originalPrice * discountPercent);
+        return newPrice + newPrice*salesTax;
     }
 
     /**
